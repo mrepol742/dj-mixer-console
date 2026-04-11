295a4f12b8b48ccb9e3109ac1d1ff51ca9f16497
Melvin Jones Repol <mrepol742@gmail.com>
2026-04-11 12:53:50 +0800

    feat: remove countdown page and dev route flag
    - delete countdown-page component (ts, html, css)
    - remove is_development from .env.example
    - simplify routes to always use consolecomponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,11 @@
 import { Routes } from '@angular/router';
 import { ConsoleComponent } from './console/console.component';
 import { Landing } from './landing-page/landing-page.component';
-import { environment } from '../environments/environment';
-import { Countdown } from './countdown-page/countdown-page.component';
 
 export const routes: Routes = [
   { path: '', component: Landing },
   {
     path: 'console',
-    component: environment.IS_DEVELOPMENT === 'true' ? ConsoleComponent : Countdown,
+    component: ConsoleComponent,
   },
 ];
